25a0b64d7df63539205c3a66c90d833afdbff917
mwojtczak001 <120602318+mwojtczak001@users.noreply.github.com>
2024-01-18 23:03:14 -0500

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <div class="footer-container">
       <img src="/Twitter_Icon.png" />
-      <img src="../public/Facebook_Icon.png" />
-      <img src="../public/Instagram_Icon.png" />
-      <img src="../public/Linkedin_Icon.png" />
-      <img src="../public/Github_icon.png" />
+      <img src="Facebook_Icon.png" />
+      <img src="Instagram_Icon.png" />
+      <img src="Linkedin_Icon.png" />
+      <img src="Github_icon.png" />
     </div>
   )
 }
