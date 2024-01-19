d6726e6687edf46444609fba14b7905be261967b
mwojtczak001 <120602318+mwojtczak001@users.noreply.github.com>
2024-01-18 23:02:28 -0500

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer() {
   return (
     <div class="footer-container">
-      <img src="../public/Twitter_Icon.png" />
+      <img src="/Twitter_Icon.png" />
       <img src="../public/Facebook_Icon.png" />
       <img src="../public/Instagram_Icon.png" />
       <img src="../public/Linkedin_Icon.png" />
